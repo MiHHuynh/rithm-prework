9356f8890f481846ff8d69aa20d4fb814c7a9643
Michelle Huynh <michhh.12@gmail.com>
2017-07-10 22:09:45 -0700

    finished function exercises

--- Rithm-Javascript-Fundamentals/function-exercises.js
+++ Rithm-Javascript-Fundamentals/function-exercises.js
@@ -389,26 +389,94 @@ console.log("This should output a TypeError. sayHi is hoisted and initiated with
 
 // this function takes in two parameters and returns the difference of the two
 
+function difference(a, b) {
+	return a-b;
+}
+
 // this function takes in two parameters and returns the product of the two
+
+function product(a, b) {
+	return a*b;
+}
 
 // this function takes in one parameter (a number from 1-7) and returns the day of the week
 // (1 is Sunday, 2 is Monday, 3 is Tuesday etc.). If the number is less than 1 or greater than 7,
 // the function should return undefined;
 
+function printDay(num) {
+	switch(num) {
+		case 1: return "Sunday";
+		case 2: return "Monday";
+		case 3: return "Tuesday";
+		case 4: return "Wednesday";
+		case 5: return "Thursday";
+		case 6: return "Friday";
+		case 7: return "Saturday";
+		default: return undefined;
+	}
+}
+
 // this function takes in one parameter (an array) and returns the last value in the array. 
 // It should return undefined if the array is empty.
+
+function lastElement(arr) {
+	if (arr.length === 0) {
+		return undefined;
+	} else {
+		return arr[arr.length-1];
+	}
+}
 
 // this function takes in two parameters (both numbers). If the first is greater than the second, 
 // this function returns "First is greater". If the second number is greater than the first, the 
 // function returns "Second is greater". Otherwise the function returns "Numbers are equal"
+
+function numberCompare(a, b) {
+	if (a > b) {
+		return "First is greater.";
+	} else if (b > a) {
+		return "Second is greater.";
+	} else {
+		return "Numbers are equal.";
+	}
+}
 
 // this function takes in two parameters (two strings). The first parameter should be a word 
 // and the second should be a letter. The function returns the number of times that letter appears
 // in the word. The function should be case insensitive (does not matter if the input is lowercase
 // or uppercase). If the letter is not found in the word, the function should return 0.
 
+function singleLetterCount(str, letter) {
+	var count = 0;
+	for (var i = 0; i < str.length; i++) {
+		if (str[i].toLowerCase() === letter.toLowerCase()) {
+			count++;
+		}
+	}
+	return count;
+}
+
 // this function takes in one parameter (a string) and returns an object with the keys being the
 // letters and the values being the count of the letter.
+
+function multipleLetterCount(str) {
+	var obj = {};
+	for (var i = 0; i < str.length; i++) {
+		/* 
+		loop through string
+		hits first letter --> count[first letter] = 1
+		hits second letter --> count[second letter] = 1, etc.
+		if letter already exists in object, increment
+		else, count[current letter] = 1.
+		*/
+		if (str[i] in obj) {
+			obj[str[i]] += 1;
+		} else {
+			obj[str[i]] = 1;
+		}
+	}
+	return obj;
+}
 
 // this function should take in at most four parameters (an array, command, location, and value).
 // If the command is "remove" and the location is "end", the function should remove the last
@@ -420,20 +488,85 @@ console.log("This should output a TypeError. sayHi is hoisted and initiated with
 // If the command is "add" and the location is "end", the function should add the value
 // (fourth parameter) to the end of the array and return the array.
 
+function arrayManipulation (arr, command, location, value) {
+	if (command === "add") {
+		if (location === "beginning") {
+			arr.unshift(value);
+		}
+		if (location === "end") {
+			arr.push(value);
+		}
+	}
+	if (command === "remove") {
+		if (location === "beginning") {
+			arr.shift();
+		}
+		if (location === "end") {
+			arr.pop();
+		}
+	}
+	return arr;
+}
+
 // A Palindrome is a word, phrase, number, or other sequence of characters which reads the same
 // backward or forward. This function should take in one parameter and returns true or false if it
 // is a palindrome. As a bonus, allow your function to ignore whitespace and capitalization so that
 // isPalindrome('a man a plan a canal Panama'); returns true
 
+function isPalindrome(str) {
+	str = str.toLowerCase().split(" ").join("").split("");
+	//"asdf ASDF" --> [a,s,d,f,a,s,d,f]
+
+	var reverse = [];
+	for (var i = str.length-1; i >= 0; i--) {
+		reverse.push(str[i]);
+	}
+	str = str.join("");
+	reverse = reverse.join("");
+	if (reverse === str) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 // using your knowledge so far, build a game of Rock/Paper/Scissor where through the use of the prompt
 // function, a user can enter their choice and based on a random selection - they can either tie/win or lose
 // against a computer.
 
+function rockPaperScissors() {
+	var choices = ["rock", "paper", "scissors"];
+	var userChoice = prompt("Type rock, paper, or scissors:").toLowerCase();
+	var computerChoice = choices[(Math.floor((Math.random() * 3) + 1)) - 1];
+	var results = ["You win.", "You lose.", "It's a tie."];
+	var endMessage = "You played " + userChoice + ". The computer played " + computerChoice + ". ";
+	if (userChoice === computerChoice) {
+		return endMessage + results[2];
+	}
+	if (userChoice === choices[0]) { //if user played ROCK
+		if (computerChoice === choices[1]) { //computer plays PAPER
+			return endMessage + results[1];
+		}
+		if (computerChoice === choices[2]) { //computer plays SCISSORS
+			return endMessage + results[0];
+		}
+	}
+	if (userChoice === choices[1]) { //if user played PAPER
+		if (computerChoice === choices[0]) { //computer plays ROCK
+			return endMessage + results[0];
+		}
+		if (computerChoice === choices[2]) { //computer playes SCISSORS
+			return endMessage + results[1];
+		}
 
-
-
-
-
-
-
-
+	}
+	if (userChoice === choices[2]) { //if user played SCISSORS
+		if (computerChoice === choices[0]) { //computer plays ROCK
+			return endMessage + results[1];
+		}
+		if (computerChoice === choices[1]) { //computer plays PAPER
+			return endMessage + results[0];
+		}
+	}
+	return "Please try again and enter a valid option.";
+}
