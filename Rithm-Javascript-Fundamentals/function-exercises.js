83f8d0cfdcce1161e9bd6f36e34a3698374346be
Michelle Huynh <michhh.12@gmail.com>
2017-07-10 19:23:33 -0700

    added text for function exercises

--- Rithm-Javascript-Fundamentals/function-exercises.js
+++ Rithm-Javascript-Fundamentals/function-exercises.js
@@ -313,6 +313,8 @@ displayFirstName();
 console.log(firstName) // ?
 */
 
+console.log("The above code should output undefined. The console.log is trying to access a variable that was defined only within the scope of the function displayFirstName.");
+
 /*
 What does the following code output? Why?
 
@@ -320,11 +322,18 @@ console.log(firstName) // ?
 var firstName = "Elie"
 */
 
+console.log("The following code outputs undefined due to hoisting. firstName only becomes \"Elie\" after the console.log.");
+
 /*
 What does the following code output? Why?
 
 console.log(firstName) // ?
 firstName = 'Elie'
+*/
+
+console.log("The following code outputs a ReferenceError. Without the var keyword, variables do not get hoisted, thus making the firstName variable nonexistent at the time that it is used.");
+
+/*
 What does the following code output? Why?
 
 function sayHi(){
@@ -334,6 +343,8 @@ function sayHi(){
 
 sayHi(); // ?
 */
+
+console.log("The following code outputs \"Hiundefined\" due to hoisting. firstName will be hoisted to the top of its scope and won't be assigned until after the return.");
 
 /*
 What does the following code output? Why?
@@ -346,6 +357,8 @@ function sayHi(){
 sayHi(); // ?
 */
 
+console.log("The code should output a ReferenceError. firstName is declared without the var keyword, so it does not get hoisted, and thus is nonexistent at the time it is used in the return statement.");
+
 /*
 What does the following code output? Why?
 
@@ -355,6 +368,8 @@ function sayHi(){
     return 'Hi!';
 }
 */
+
+console.log("This should output \'Hi!\' because functions are always hoisted.");
 
 /*
 What does the following code output? Why?
@@ -366,6 +381,59 @@ var sayHi = function(){
 }
 */
 
+console.log("This should output a TypeError. sayHi is hoisted and initiated with the value of undefined. At the time that it is invoked like a function, it is currently undefined, which cannot be called like a function.");
+
 /*-----------*/
 
 // { Functions Exercises. }
+
+// this function takes in two parameters and returns the difference of the two
+
+// this function takes in two parameters and returns the product of the two
+
+// this function takes in one parameter (a number from 1-7) and returns the day of the week
+// (1 is Sunday, 2 is Monday, 3 is Tuesday etc.). If the number is less than 1 or greater than 7,
+// the function should return undefined;
+
+// this function takes in one parameter (an array) and returns the last value in the array. 
+// It should return undefined if the array is empty.
+
+// this function takes in two parameters (both numbers). If the first is greater than the second, 
+// this function returns "First is greater". If the second number is greater than the first, the 
+// function returns "Second is greater". Otherwise the function returns "Numbers are equal"
+
+// this function takes in two parameters (two strings). The first parameter should be a word 
+// and the second should be a letter. The function returns the number of times that letter appears
+// in the word. The function should be case insensitive (does not matter if the input is lowercase
+// or uppercase). If the letter is not found in the word, the function should return 0.
+
+// this function takes in one parameter (a string) and returns an object with the keys being the
+// letters and the values being the count of the letter.
+
+// this function should take in at most four parameters (an array, command, location, and value).
+// If the command is "remove" and the location is "end", the function should remove the last
+// value in the array and return the value removed. (In this case, the function only needs three arguments.)
+// If the command is "remove" and the location is "beginning", the function should remove the
+// first value in the array and return the value removed. (In this case, the function only needs three arguments.)
+// If the command is "add" and the location is "beginning", the function should add the value
+// (fourth parameter) to the beginning of the array and return the array.
+// If the command is "add" and the location is "end", the function should add the value
+// (fourth parameter) to the end of the array and return the array.
+
+// A Palindrome is a word, phrase, number, or other sequence of characters which reads the same
+// backward or forward. This function should take in one parameter and returns true or false if it
+// is a palindrome. As a bonus, allow your function to ignore whitespace and capitalization so that
+// isPalindrome('a man a plan a canal Panama'); returns true
+
+// using your knowledge so far, build a game of Rock/Paper/Scissor where through the use of the prompt
+// function, a user can enter their choice and based on a random selection - they can either tie/win or lose
+// against a computer.
+
+
+
+
+
+
+
+
+
