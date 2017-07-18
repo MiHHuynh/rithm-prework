33858f535b27d394cdeaf9c12f6d953737c00b36
Michelle Huynh <michhh.12@gmail.com>
2017-07-18 14:20:54 -0700

    finished some of the part 3 iterators exercises

--- Rithm-Intermediate-JS-2/iterators-exercises.js
+++ Rithm-Intermediate-JS-2/iterators-exercises.js
@@ -219,52 +219,116 @@ function removeVowels(str) {
 
 // Write a function called listNames which takes in an array of songs and console.logs the name of each one.
 
+function listNames(arrSongs) {
+	arrSongs.forEach(function(song){
+		console.log(song.name);
+	});
+}
+
 // Write a function called listSongDetails which takes in an array of songs and console.logs details about
 // each one. The details should be in the following example format: "Smooth, by Santana featuring Rob Thomas (4:00)".
+
+function listSongDetails(arrSongs) {
+	arrSongs.forEach(function(song){
+		console.log(song.name + ", by " + song.artist + " " + song.duration);
+	});
+}
 
 // Write a function called summerJamCount which takes in an array of songs and returns the number of songs
 // which hit #1 on the charts in June, July, or August.
 
+function summerJamCount(arrSongs) {
+	var count = 0;
+	var summerMonths = [6, 7, 8];
+	arrSongs.forEach(function(song){
+		if (summerMonths.includes(song.month)) {
+			count++;
+		}
+	});
+	return count;
+}
+
 // Write a function called getDurations which takes in an array of songs and returns an array of each song's duration.
+
+function getDurations(arrSongs) {
+	return arrSongs.map(function(song){
+		return song.duration;
+	});
+}
+
 
 // Write a function called getDurationInSeconds which takes in an array of songs and returns an array of each
 // song's duration in seconds.
+
+function getDurationInSeconds(arrSongs) {
+	return arrSongs.map(function(song){
+		// parseInt only gets the first digits before hitting a non-number character
+		// this makes it easy because we can get the number of min, multiply by 60, and
+		// then grab the remaining seconds from the end of the string to add to the total
+		return (parseInt(song.duration, 10) * 60 + parseInt(song.duration.slice(-2), 10));
+	});
+}
 
 // Write a function called getMainArtists which takes in an array of songs and returns an array of the primary
 // artists on the recordings. If there's only one artist, that artist should be returned; if there are featured artists,
 // they should be ignored (so only the artist to the left of "featuring" is kept.)
 
+function getMainArtists(arrSongs) {
+	// map
+}
+
 // Write a function called getBigHits which takes an array of songs and returns an array of songs which were number one
 // for 10 or more weeks.
+
+function getBigHits(arrSongs) {
+	// filter
+}
 
 // Write a function called getShortSongs which takes an array of songs and returns an array of songs which shorter
 // than 3 minutes.
 
+function getShortSongs(arrSongs) {
+	// filter
+}
+
 // Write a function called getSongsByArtist which takes in an array of artists and the name of an artist and returns
 // an array of songs by that artist.
 
+function getSongsByArtist(arrSongs) {
+	// filter
+}
+
 // Write a function called getOneHitWonders which takes in an array of songs and returns an array of songs for artists
-// who only appe
+// who only appear once in the original array.
+
+function getOneHitWonders(arrSongs) {
+	// filter
+}
 
 // Refactor summerJamCount to use reduce!
+
+function summerJamCount(arrSongs) {
+	// reduce
+}
 
 // Write a function called getTotalDurationInSeconds which takes in an array of songs and returns the total amount of
 // time (in seconds) it would take to listen to all of the songs. (Hint: can you use anything you've written already
 // to help solve this problem?)
 
+function getTotalDurationInSeconds(arrSongs) {
+	// reduce
+}
+
 // Write a function called getSongCountByArtist which takes in an array of songs and returns an object. The keys in the
 // object should be artist names, and the values should be the number of songs by that artist in the orignal array.
+
+function getSongCountByArtist(arrSongs) {
+	// reduce
+}
 
 // Write a function called averageWeeksAtNumberOne which takes in an array of songs and returns the average number of
 // weeks that songs on the list were #1.
 
-
-
-
-
-
-
-
-
-
-
+function averageWeeksAtNumberOne(arrSongs) {
+	// reduce
+}
