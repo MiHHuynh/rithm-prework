834da73e986cf2078f331c670ccf483d1ae1f9bd
Michelle Huynh <michhh.12@gmail.com>
2017-07-20 00:20:53 -0700

    finished to do list

--- Rithm-Intermediate-JS-1/events-exercises/part3-4_to-do-list/script.js
+++ Rithm-Intermediate-JS-1/events-exercises/part3-4_to-do-list/script.js
@@ -1,74 +1,88 @@
 document.addEventListener("DOMContentLoaded", function(event){
 	// retrieve anything that has been stored in local storage
-	var incompleteItems = JSON.parse(localStorage.getItem("incompleteItems"));
+	var items = JSON.parse(localStorage.getItem("items"));
+	console.log(items);
 
 	// if it's not empty, populate the to-do list with whatever's available
-	if (incompleteItems != null && incompleteItems.length > 0) {
-		for (var i = 0; i < incompleteItems.length; i++) {
-			var itemDiv = document.createElement("div");
-			itemDiv.innerText = incompleteItems[i];
-			document.getElementById("to-do-list").appendChild(itemDiv);
-		}
+	if (items !== null && items.length > 0) {
+		items.forEach(function(item){
+			var itemLi = document.createElement("li");
+			itemLi.innerText = item.name;
+			itemLi.setAttribute("id", item.id);
+			if (item.completed === true) {
+				itemLi.style.textDecoration = "line-through";
+			}
+			document.getElementById("to-do-list").appendChild(itemLi);
+			createDeleteButton(itemLi);
+		});
 	} else {
-		incompleteItems = [];
-	}
-
-	// retrieve and populate completed items
-	var finishedItems = JSON.parse(localStorage.getItem("doneItems"));
-	if (finishedItems != null && finishedItems.length > 0) {
-		for (var i = 0; i < finishedItems.length; i++) {
-			var doneDiv = document.createElement("div");
-			doneDiv.innerText = finishedItems[i];
-			doneDiv.setAttribute("class", "done");
-			doneDiv.style.textDecoration = "line-through";
-			document.getElementById("body").appendChild(doneDiv);
-		}
-	} else {
-		finishedItems = [];
+		items = [];
 	}
 
 	var addButton = document.getElementById("add-item");
 	addButton.addEventListener("click", function(event) {
+
 		// add item to HTML list
 		var item = document.getElementById("item").value;
-		var toDoItem = document.createElement("div");
+		var toDoItem = document.createElement("li");
 		toDoItem.innerText = item;
-		toDoItem.setAttribute("class", "incomplete-item");
+		toDoItem.setAttribute("id", Date());
 		document.getElementById("to-do-list").appendChild(toDoItem);
 		
-		// give item a delete button
-		var deleteButton = document.createElement("button");
-		deleteButton.setAttribute("class", "deleteButton");
-		deleteButton.innerText = "Done?";
-		toDoItem.appendChild(deleteButton);
-
 		// clear input field
 		document.getElementById("item").value = "";
 
-		// add item to array for localStorage
-		incompleteItems.push(item);
-		localStorage.setItem("incompleteItems", JSON.stringify(incompleteItems));
+		createDeleteButton(toDoItem);
 
+		// add item to array for localStorage
+		var itemObj = {
+			id: Date(),
+			name: item,
+			completed: false
+		};
+		items.push(itemObj);
+		localStorage.setItem("items", JSON.stringify(items));
+		console.log(items);
 	});
 
 	// strike-through list items when clicked, or remove completely if remove button is clicked
-	document.querySelector("body").addEventListener("click", function(event){
-		if (event.target.classList.contains("incomplete-item" || event.target.toLowerCase() === "div")) {
+	var toDoList = document.getElementById("to-do-list");
+	toDoList.addEventListener("click", function(event){
+		if (event.target.tagName.toLowerCase() === "li") {
 			event.target.style.textDecoration = "line-through";
-			finishedItems.push(event.target.innerText);
-			console.log(event.target.innerText);
+
+			// update in items array that item is completed
+			var itemID = event.target.id;
+			items.forEach(function(item){
+				if (item.id === itemID) {
+					item.completed = true;
+				}
+			});
+			localStorage.setItem("items", JSON.stringify(items));
+			console.log(items);
 		} else if (event.target.classList.contains("deleteButton")) {
-			event.target.previousSibling.remove();
-			event.target.remove();
+			event.target.parentNode.remove();
+
+			// remove item completely from items array
+			var itemID = event.target.ID;
+			var itemIndex = items.find(function(item){
+				return item.id === itemID;
+			});
+			items.splice(itemIndex, 1);
+			localStorage.setItem("items", JSON.stringify(items));
+			console.log(items);
 		}
 	});
 
-// need to update incomplete or complete lists, repopulate with proper formatting
-// e.g. line through or not
-// probably change the data structure of the items to a single array of objects
+	// store array of item objects into localStorage
+	localStorage.setItem("items", JSON.stringify(items));
 
 });
 
-function appendDeleteButton() {
-
-};
+function createDeleteButton(item) {
+	// give item a delete button
+	var deleteButton = document.createElement("button");
+	deleteButton.setAttribute("class", "deleteButton");
+	deleteButton.innerText = "X";
+	item.appendChild(deleteButton);
+}
