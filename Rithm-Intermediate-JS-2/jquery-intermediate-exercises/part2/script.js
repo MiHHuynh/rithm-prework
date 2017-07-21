9fd00efb3227a62c44d946236aebdf9031028fda
Michelle Huynh <michhh.12@gmail.com>
2017-07-21 13:47:15 -0700

    finished jquery intermediate exercise part 2

---
+++ Rithm-Intermediate-JS-2/jquery-intermediate-exercises/part2/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$("#add-item").on("click", function(event){
+		event.preventDefault();
+		var food = $("#food-field").val();
+		var rating = $("#rating-field").val();
+
+		// add a new row with the food, rating, and button to table
+		var $row = $("<tr><td>" + food + "</td><td>" + rating + "</td><td><button class=\"btn btn-danger delete-row\">Delete</button></td></tr>")
+		$("tbody").append($row);
+		resetFields();
+	});
+
+	// when delete button is pressed, remove row
+	var $deleteRowButton = $(".delete-row");
+	$("table").on("click", $deleteRowButton, function(event){
+		$(event.target).parent().prevAll().parent().remove();
+	});
+});
+
+function resetFields() {
+	$("#food-field").val("");
+	$("#rating-field").val("");
+}
